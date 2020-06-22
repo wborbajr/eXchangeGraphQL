bb7a5b7328592aee3057c0839804514df139cce4
Waldir Borba Junior <wborbajr@gmail.com>
2020-06-22 18:20:35 -0300

    feat: init import

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# eXchangeGraphQL
+# Setup a GraphQL API with Apollo 2.0 Sequelize and Express.js
 
-eXchange Refactored to GraphQL
+## Instructions
+
+Please do follow the instructions in the article published on [medium](https://medium.com/infocentric/setup-a-graphql-api-with-apollo-2-0-sequelize-and-express-js-608d1365d776).
+
+"start": "npm run init-db && npx nodemon --exec babel-node server.js",
---
+++ server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+import faker from "faker";
+import times from "lodash.times";
+import random from "lodash.random";
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
+import db from "./models";
+
+const playground = {
+  settings: {
+    "editor.cursorShape": "line"
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs: gql(typeDefs),
+  resolvers,
+  context: { db },
+  playground
+});
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.use(express.static("app/public"));
+
+db.sequelize.sync().then(() => {
+  // populate author table with dummy data
+  db.author.bulkCreate(
+    times(10, () => ({
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName()
+    }))
+  );
+  // populate post table with dummy data
+  db.post.bulkCreate(
+    times(10, () => ({
+      title: faker.lorem.sentence(),
+      content: faker.lorem.paragraph(),
+      authorId: random(1, 10)
+    }))
+  );
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+});
